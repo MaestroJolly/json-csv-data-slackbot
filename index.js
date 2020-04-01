49f9a6045c81b237f10e244b40ce4899acd87033
Yusuf Jolaoso <jolaosoyusuf@gmail.com>
2020-04-01 17:49:32 +0100

    Update: csv functionality and AWS S3 implemented

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ require('dotenv').config();
 
 const bodyParser = require('body-parser');
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const app = express();
-const PORT = process.env.PORT || 3333;
 const datasets = require('./datasets');
+const { Parser } = require('json2csv');
+const aws = require('aws-sdk');
+
+const PORT = process.env.PORT || 3333;
+const S3_BUCKET = process.env.S3_BUCKET;
+
+aws.config.region = 'us-east-2';
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // use morgan logger middleware
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res)=>{
     res.json({
@@ -38,9 +44,50 @@ app.post('/webhook', (req, res)=>{
             })
         }
     }else if(params[0] === "sheet"){
-        res.json({
-            "text": "Work In Progress"
-        })
+        const fetchData = datasets(parseInt(params[1]));
+        if (fetchData){
+            const fields = ['ID', 'NAME', 'HABITAT'];
+            const json2csvParser = new Parser({ fields });
+            const animalList = {'ID': params[1], 'NAME': fetchData.name, 'HABITAT': fetchData.habitat}
+            const csv = json2csvParser.parse(animalList);
+            const s3 = new aws.S3();
+            const fileName = 'animals/animal-' + Date.now() + '-' + params[1] + '.csv';
+
+            const s3Params = {
+                Bucket: S3_BUCKET,
+                Key: fileName,
+                ACL: 'public-read',
+                Body: csv
+            };
+
+            // May be needed to handle space complexity
+            // s3.getSignedUrl('putObject', s3Params, (err, data) => {
+            //     if(err){
+            //     console.log(err);
+            //     return res.end();
+            //     }
+            //     res.json({
+            //         "text": `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+            //     })
+            // });
+
+            // call S3 to retrieve upload file to specified bucket
+            s3.putObject(s3Params, function (err, data) {
+                if (err) {
+                    console.log("Error", err);
+                } if (data) {
+                    console.log("Upload Success", data);
+                    res.json({
+                        "text": `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+                    })
+                }
+            });
+        }else{
+            res.json({
+                "text": "No data found"
+            })
+        }
+        
     }else{
         res.json({
             "text": "No found option"
